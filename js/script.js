ee0fb673182a42c3dca56eb4f0932539b4a056d5
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-08 20:49:49 +0600

    selection oparation

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("hellow from JS");
+const tagName = document.getElementsByTagName("li");
+//   console.log(tagName);
+for (const li of tagName) {
+  //   console.log(li.innerHTML);
+}
+const persons = document.getElementsByClassName("person-name");
+for (const person of persons) {
+  console.log(person.innerText);
+}
+
+const uniqeName =
+  ((document.getElementById("jamal").style.color = "red"),
+  (document.getElementById("jamal").style.backgroundColor = "green"),
+  (backgroundColor = "blue"));
+uniqeName.innerText = "kire mama ki khobor";
